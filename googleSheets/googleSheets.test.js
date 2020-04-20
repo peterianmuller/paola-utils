c57796e8123f82ffde12200b823706f84ab9e3ab
Murph Grainger <murphgrainger@Murphs-Macbook.local>
2020-04-20 17:35:31 -0600

    removed fake gsheets test

--- googleSheets/googleSheets.test.js
+++
@@ -1,6 +0,0 @@
-const { readAllRows } = require('.');
-
-test('Should asynchronously lie about reading all of the rows', async () => {
-    const greeting = await readAllRows();
-    expect(greeting).toBe('Reading all the rows! :P');
-});
