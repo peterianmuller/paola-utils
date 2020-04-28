bda9fca095de4ee6409a5bbffcd2df14a009a848
Murph Grainger <murphgrainger@Murphs-Macbook.local>
2020-04-27 18:00:48 -0600

    put tests in consistent order and reformatted from code review

--- github/github.test.js
+++ github/github.test.js
@@ -8,74 +8,80 @@ const {
   removeUserFromTeam,
 } = require('.');
 
-const GITHUB_TEAM_HANDLE = 'paola-test-team';
+const GITHUB_TEAM_USERNAME = 'paola-test-team';
 const GITHUB_TEST_USER = 'murphpaolatestuser';
 const GITHUB_INVALID_USER = 'notarealuser***';
 const HEADERS = { Authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}` };
 
-function addUser() {
-  return fetch(`${GITHUB_API_TEAMS}/${GITHUB_TEAM_HANDLE}/memberships/${GITHUB_TEST_USER}`, { method: 'PUT', headers: HEADERS });
-}
+const addUser = async () => {
+  await fetch(
+    `${GITHUB_API_TEAMS}/${GITHUB_TEAM_USERNAME}/memberships/${GITHUB_TEST_USER}`,
+    { method: 'PUT', headers: HEADERS },
+  );
+};
 
-function removeUser() {
-  return fetch(`${GITHUB_API_TEAMS}/${GITHUB_TEAM_HANDLE}/memberships/${GITHUB_TEST_USER}`, { method: 'DELETE', headers: HEADERS });
-}
+const removeUser = async () => {
+  await fetch(
+    `${GITHUB_API_TEAMS}/${GITHUB_TEAM_USERNAME}/memberships/${GITHUB_TEST_USER}`,
+    { method: 'DELETE', headers: HEADERS },
+  );
+};
 
 beforeAll(() => {
   removeUser();
 });
 
 describe('validateUser', () => {
-  test('Should return false if invalid github user', async () => {
-    const isValid = await validateUser(GITHUB_INVALID_USER);
-    expect(isValid).toBe(false);
-  });
-
   test('Should return true if valid github user', async () => {
     const isValid = await validateUser(GITHUB_TEST_USER);
     expect(isValid).toBe(true);
   });
+
+  test('Should return false if invalid github user', async () => {
+    const isValid = await validateUser(GITHUB_INVALID_USER);
+    expect(isValid).toBe(false);
+  });
 });
 
 describe('isUserOnTeam', () => {
+  test('Should return true if user is on team', async () => {
+    await addUser();
+    const userOnTeam = await isUserOnTeam(GITHUB_TEST_USER, GITHUB_TEAM_USERNAME);
+    expect(userOnTeam).toBe(true);
+  });
+
   test('Should return false if user is not on team', async () => {
-    const userOnTeam = await isUserOnTeam(GITHUB_TEST_USER, GITHUB_TEAM_HANDLE);
+    await removeUser();
+    const userOnTeam = await isUserOnTeam(GITHUB_TEST_USER, GITHUB_TEAM_USERNAME);
     expect(userOnTeam).toBe(false);
   });
 
   test('Should return false if user does not exist', async () => {
-    const userOnTeam = await isUserOnTeam(GITHUB_INVALID_USER, GITHUB_TEAM_HANDLE);
+    const userOnTeam = await isUserOnTeam(GITHUB_INVALID_USER, GITHUB_TEAM_USERNAME);
     expect(userOnTeam).toBe(false);
-  });
-
-  test('Should return true if user is on team', async () => {
-    await addUser();
-    const userOnTeam = await isUserOnTeam(GITHUB_TEST_USER, GITHUB_TEAM_HANDLE);
-    expect(userOnTeam).toBe(true);
   });
 });
 
 describe('addUserToTeam', () => {
-  test('Should return false if user does not exist', async () => {
-    const addedUser = await addUserToTeam(GITHUB_INVALID_USER, GITHUB_TEAM_HANDLE);
-    expect(addedUser).toBe(false);
-  });
-
   test('Should return true if successfully added user', async () => {
     await removeUser();
-    const addedUser = await addUserToTeam(GITHUB_TEST_USER, GITHUB_TEAM_HANDLE);
+    const addedUser = await addUserToTeam(GITHUB_TEST_USER, GITHUB_TEAM_USERNAME);
     expect(addedUser).toBe(true);
+  });
+  test('Should return false if user does not exist', async () => {
+    const addedUser = await addUserToTeam(GITHUB_INVALID_USER, GITHUB_TEAM_USERNAME);
+    expect(addedUser).toBe(false);
   });
 });
 
 describe('removeUserFromTeam', () => {
   test('Should return true if successfully removed user', async () => {
-    const removed = await removeUserFromTeam(GITHUB_TEST_USER, GITHUB_TEAM_HANDLE);
+    const removed = await removeUserFromTeam(GITHUB_TEST_USER, GITHUB_TEAM_USERNAME);
     expect(removed).toBe(true);
   });
 
   test('Should return false if user is invalid', async () => {
-    const removed = await removeUserFromTeam(GITHUB_INVALID_USER, GITHUB_TEAM_HANDLE);
+    const removed = await removeUserFromTeam(GITHUB_INVALID_USER, GITHUB_TEAM_USERNAME);
     expect(removed).toBe(false);
   });
 });
--- github/index.js
+++ github/index.js
@@ -7,40 +7,52 @@ const { GITHUB_API_USERS, GITHUB_API_TEAMS } = require('../constants');
 
 const headers = { Authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}` };
 
-// Validate a github handle (verify it exists) - return boolean
-exports.validateUser = async (handle) => {
+// Validate a github username (verify it exists) - return boolean
+exports.validateUser = async (username) => {
   try {
-    const response = await fetch(`${GITHUB_API_USERS}/${handle}`, { headers });
+    const response = await fetch(
+      `${GITHUB_API_USERS}/${username}`,
+      { headers },
+    );
     return response.status === 200;
   } catch (error) {
     return error;
   }
 };
 
-// Determine if handle is member of provided team (one version)
-exports.isUserOnTeam = async (handle, team) => {
+// Determine if username is member of provided team (one version)
+exports.isUserOnTeam = async (username, team) => {
   try {
-    const response = await fetch(`${GITHUB_API_TEAMS}/${team}/memberships/${handle}`, { headers });
+    const response = await fetch(
+      `${GITHUB_API_TEAMS}/${team}/memberships/${username}`,
+      { headers },
+    );
     return response.status === 200;
   } catch (error) {
     return error;
   }
 };
 
-// Add a github handle to a github team
-exports.addUserToTeam = async (handle, team) => {
+// Add a github username to a github team
+exports.addUserToTeam = async (username, team) => {
   try {
-    const response = await fetch(`${GITHUB_API_TEAMS}/${team}/memberships/${handle}`, { method: 'PUT', headers });
+    const response = await fetch(
+      `${GITHUB_API_TEAMS}/${team}/memberships/${username}`,
+      { method: 'PUT', headers },
+    );
     return response.status === 200;
   } catch (error) {
     return error;
   }
 };
 
-// Delete a handle from a team
-exports.removeUserFromTeam = async (handle, team) => {
+// Delete a username from a team
+exports.removeUserFromTeam = async (username, team) => {
   try {
-    const response = await fetch(`${GITHUB_API_TEAMS}/${team}/memberships/${handle}`, { method: 'DELETE', headers });
+    const response = await fetch(
+      `${GITHUB_API_TEAMS}/${team}/memberships/${username}`,
+      { method: 'DELETE', headers },
+    );
     return response.status === 204;
   } catch (error) {
     return error;
