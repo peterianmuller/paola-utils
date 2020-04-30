84f3360bb7567c2219ef4883935e57d1706c9fc5
Murph Grainger <murphgrainger@Murphs-Macbook.local>
2020-04-30 11:38:03 -0600

    added initial function to query sfdc

--- salesforce/index.js
+++ salesforce/index.js
@@ -1,15 +1,35 @@
-// const { SALESFORCE_API_KEY } = require('./config.js');
+require('dotenv').config();
+const jsforce = require('jsforce');
+
+const { QUERY_SELECT, QUERY_WHERE, DATA_REFORMAT } = require('./query-mapping');
+const conn = new jsforce.Connection({ loginUrl: process.env.SFDC_LOGIN_URL });
+
 // ------------------------------
 // Salesforce API Integrations
 // ------------------------------
 
-// Read a sales force report
-exports.getReport = async () => {
-
+const login = async () => {
+  try {
+    return await conn.login(process.env.SFDC_USERNAME, process.env.SFDC_PASSWORD,
+      (err, userInfo) => userInfo);
+  } catch (error) {
+    return error;
+  }
 };
 
-// TODO: Move this elsewhere! You can skip this one for now.
-// Group students in a report by (a number of parameters TBD)
-exports.groupStudentsBy = () => {
-
+exports.getStudents = async () => {
+  try {
+    await login();
+    return await conn.sobject('Opportunity')
+      .select(QUERY_SELECT)
+      .where(QUERY_WHERE)
+      .orderby('CreatedDate', 'DESC')
+      .execute((err, res) => {
+        if (err) throw new Error('SALESFORCE ERROR', err);
+        const formattedStudentData = DATA_REFORMAT(res);
+        return formattedStudentData;
+      });
+  } catch (error) {
+    return error;
+  }
 };
