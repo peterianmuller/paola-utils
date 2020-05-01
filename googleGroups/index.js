e3e675f59d490bcbe947be11e377d371899322bb
Murph Grainger <murphgrainger@Murphs-Macbook.local>
2020-05-01 13:24:52 -0600

    initial auth attempt for google troups

--- googleGroups/index.js
+++ googleGroups/index.js
@@ -7,34 +7,32 @@ const { google } = require('googleapis');
 const key = require('../../google/admin_sdk_client_secret.json');
 
 const scopes = 'https://www.googleapis.com/auth/admin.directory.group';
-const jwt = new google.auth.JWT(key.client_email, null, key.private_key);
-const viewId = 'XXXXXXX';
+const jwt = new google.auth.JWT(key.client_email, null, key.private_key, scopes, 'murph.grainger@galvanize.com');
 
 // Write a student to a group
 exports.addGroupMember = async () => {
   try {
-    await jwt.authorize((err, response) => {
-      console.log(response);
-      return response;
+    await jwt.authorize((err, tokens) => {
+      if (err) return;
+      console.log('Successfully connected!');
     });
-    const data = await google.groupssettings().get(
-      {
-        auth: jwt,
-        ids: `ga:${viewId}`,
-        'start-date': '30daysAgo',
-        'end-date': 'today',
-        metrics: 'ga:pageviews',
-      },
-      (err, result) => {
-        console.log(err, result);
-      },
-    );
+    await listUsers(jwt);
   } catch (error) {
     console.log(error);
     return error;
   }
   return 'yay!';
 };
+
+const listUsers = async (auth) => {
+  const service = await google.admin({ version: 'directory_v1', auth });
+  service.groups.get({
+    groupKey: 'paola-sandbox@galvanize.com',
+  }, (err, res) => {
+    if (err) return console.error('The API returned an error:', err.message);
+    console.log('res', res);
+  });
+}
 
 // Delete a student from a group
 exports.removeGroupMember = async () => {
