3fcf9586c543c22c1e68c9fbae7dad15fa036659
Murph Grainger <murphgrainger@Murphs-Macbook.local>
2020-04-30 18:35:18 -0600

    added group settings

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const GitHub = require('./github');
 const Learn = require('./learn');
 const Slack = require('./slack');
 
+GGroups.addGroupMember()
+
 // ------------------------------
 // API Integrations
 // ------------------------------
