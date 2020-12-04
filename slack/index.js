c7658c4aeb4ad8caa8ed8dac01f1ec5dad85569c
Peter Muller <petermuller@its-MacBook-Pro.local>
2020-12-04 12:42:49 -0800

    Reduce slack/index.js to single sendMessage method

--- slack/index.js
+++ slack/index.js
@@ -2,6 +2,15 @@ const fetch = require('node-fetch');
 const https = require('https');
 
 const token = process.env.SLACK_TOKEN;
+let delay = 300;
+
+
+// ------------------------------
+// Slack API Integrations (WIP)
+// ------------------------------
+
+// NOTE: Let's save these for a future sprint. I might take on the Slack API stuff for this
+//       iteration.
 
 // Send a message to a channel
 exports.sendMessageToChannel = async (channel, text) => {
